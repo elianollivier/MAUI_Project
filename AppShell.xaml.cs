0f7e3e4cf05ff710ca6206762e0c27649b47bced
elianollivier <elian.ollivier@etu.unilim.fr>
2025-03-22 12:00:59 +0100

    ajout des fichiers

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Elian_App;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Elian_App;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
