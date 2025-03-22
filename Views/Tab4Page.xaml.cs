4b00ef8cd9c5a68db2477e1e5a84f31f07727f3e
elianollivier <elian.ollivier@etu.unilim.fr>
2025-03-22 16:07:19 +0100

    Partie 6 finie

--- Views/Tab4Page.xaml.cs
+++ Views/Tab4Page.xaml.cs
@@ -2,8 +2,28 @@ namespace Elian_App.Views;
 
 public partial class Tab4Page : ContentPage
 {
+    private bool _isClockRunning;
+
     public Tab4Page()
     {
         InitializeComponent();
+    }
+
+    private async void OnStartClockButtonClicked(object sender, EventArgs e)
+    {
+        _isClockRunning = !_isClockRunning;
+        ((Button)sender).Text = _isClockRunning ? "Arrêter l'horloge" : "Démarrer l'horloge";
+
+        while (_isClockRunning)
+        {
+            TimeLabel.Text = DateTime.Now.ToLongTimeString();
+            await Task.Delay(1000);
+        }
+    }
+
+    private void OnChangeColorClicked(object sender, EventArgs e)
+    {
+        var r = new Random();
+        BackgroundColor = Color.FromRgb(r.Next(256), r.Next(256), r.Next(256));
     }
 }
